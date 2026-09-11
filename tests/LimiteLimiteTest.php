6ab00656caa2c11ffe9670f843198fe463b21d44
Justine <blin.justine.sio@gmail.com>
2026-09-11 16:36:17 +0200

    Modifications

--- tests/LimiteLimiteTest.php
+++ tests/LimiteLimiteTest.php
@@ -2,11 +2,25 @@
 
 namespace Tests;
 
+use App\Controllers\LimiteLimite;
+use App\Models\Database;
 use PHPUnit\Framework\TestCase;
 
 class LimiteLimiteTest extends TestCase
 {
-    public function testLimiteLimiteStructure(): void {
-        
+    public function testLimiteLimiteStructure(): void
+    {
+        $database = $this->createMock(Database::class);
+        $database->method('getQuestion')->willReturn('Question test');
+        $database->method('getReponse')->willReturn('Réponse test');
+
+        $controller = new LimiteLimite($database);
+        $result = $controller->getLimiteLimite();
+
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('question', $result);
+        $this->assertArrayHasKey('reponse', $result);
+        $this->assertEquals('Question test', $result['question']);
+        $this->assertEquals('Réponse test', $result['reponse']);
     }
 }
